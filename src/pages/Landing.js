5cefa38520c271908606bb6d2116b8f9860010bf
Adrian Alexander <adrianmauricio337@gmail.com>
2021-07-31 20:35:58 -0500

    add: landing init design

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import Navbar from '../components/Navbar'
 import './styles/Landing.css'
 
@@ -6,9 +7,14 @@ function Landing() {
     <header className="Landing">
       <Navbar />
       <div className="Landing-info">
-        <h1>Adrian Salgado</h1>
-        <h2>Frontend Developer</h2>
-        <a href="">See the latest proyects I have been roking on</a>
+        <h1 className="title">Adrian Salgado</h1>
+        <h2 className="subtitle">
+          <span className="subtitle--imp">F</span>rontend
+          <span className="subtitle--imp"> D</span>eveloper
+        </h2>
+        <Link to="" className="btn btn--link">
+          See my latest proyects
+        </Link>
       </div>
     </header>
   )
